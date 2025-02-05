c0bc821ebc81b18a295d302182ddfc8cbddefc3f
SagaForge <sagaforge@mail.com>
2025-02-05 08:01:33 -0500

    Update 2small.js

--- 2small.js
+++ 2small.js
@@ -1,7 +1,7 @@
 (function() {
     // Minimum screen size required
-    const minWidth = 1090;
-    const minHeight = 680;
+    const minWidth = 1024;
+    const minHeight = 600;
 
     // URL to redirect users if their screen is too small
     const redirectUrl = "https://example.com/small-screen-warning"; // Change this to your target page
